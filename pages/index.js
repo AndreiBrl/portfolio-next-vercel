9fcf744ad048cd42455bf4c856d8ec2747fa39da
Andrei <barbutodev@gmail.com>
2022-12-16 12:10:54 -0300

    htmlBasico

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-function Home(){
-    return <div>Oi teste ahahah</div> 
-}
-export default Home
---
+++ script.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal({reset:true});
+
+ScrollReveal().reveal('.texto2',{
+
+	distance: '50px',
+	duration: 2000,
+
+	delay: 50
+});
+ScrollReveal().reveal('.perfil1',{
+	
+	distance: '50px',
+	duration: 2000,
+
+	delay: 300
+});
+ScrollReveal().reveal('.sobre',{
+	
+	distance: '50px',
+	duration: 2000,
+
+	delay: 50
+});
+ScrollReveal().reveal('.projetos',{
+	
+	distance: '50px',
+	duration: 2000,
+
+	delay: 50
+});
+ScrollReveal().reveal('.habilidades',{
+	distance: '50px',
+	duration: 2000,
+
+	delay: 50
+});
