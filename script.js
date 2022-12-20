b70196b7f6555745354485e58495df3a0c471a8b
Andrei <barbutodev@gmail.com>
2022-12-20 19:06:27 -0300

    atua

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 var lampadaAcesa = document.querySelector('.lampadaAcesa')
 var lampadaApagada = document.querySelector('.lampadaApagada')
 var perfil = document.querySelector('.perfilSessaoUm')
+var bg = document.querySelector('main')
+var tituloSessao1 = document.querySelector('.tituloSessao1 h2')
 function darkmode(){
 	lampadaAcesa.style.visibility='hidden';
 	lampadaApagada.style.visibility='visible';
 	perfil.style.visibility='hidden';
+	bg.style.background='linear-gradient(to bottom ,#272c4f 0 40%  , #190c30 50% 100%)';
+	tituloSessao1.style.color='#fce6a2'
 }
 function lightmode(){
 	lampadaAcesa.style.visibility='visible';
 	lampadaApagada.style.visibility='hidden';
 	perfil.style.visibility='visible';
+	bg.style.background='linear-gradient(to bottom ,#FFD369 0 5%  , #fce6a2 12% 100% )';
+	tituloSessao1.style.color='#2D2D2D'
 
 }
 
